ccd91347ae1309ed7a0e58ee035d32c4af35ee46
arpit-agr <arpit.agrawal2502@gmail.com>
2021-11-30 11:04:14 +0530

    Configure markdown-it, add toc plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const emphasisOverrides = require('eleventy-plugin-emphasis');
+const pluginTOC = require('eleventy-plugin-toc')
+
 module.exports = function (eleventyConfig) {
 
 
@@ -10,6 +13,41 @@ module.exports = function (eleventyConfig) {
 	//WATCH TARGET
 	eleventyConfig.addWatchTarget("./src/css/");
 
+	//MARKDOWN-IT
+	let markdownIt = require("markdown-it");
+	let markdownItAnchor = require("markdown-it-anchor");
+	let markdownItFootnote = require("markdown-it-footnote");
+	let options = {
+	  html: true,
+	  breaks: true,
+	  linkify: true
+	};
+	let opts = {
+	  permalink: true,
+		  slugify: function(s) {
+			  return encodeURIComponent(String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, '').trim().toLowerCase().replace(/\s+/g, '-'));
+		  },
+		  permalinkClass: "direct-link",
+		  permalinkSymbol: "#",
+		  level: [2,3,4] 
+	  };
+	let markdownLib = markdownIt(options).use(markdownItFootnote).use(markdownItAnchor, opts);
+	
+	eleventyConfig.setLibrary("md", markdownLib);
+  
+	//EMPHASIS OVERRIDE
+	const markdown = markdownLib;
+  
+	  eleventyConfig.addPlugin(emphasisOverrides, {
+		  'md': markdown,
+		  '_': 'mark',
+		  '__': 'b'
+	  });
+  
+	  eleventyConfig.setLibrary('md', markdown);
+  
+	  //PLUGIN
+	  eleventyConfig.addPlugin(pluginTOC);
 
 	return {
 		dir: {
